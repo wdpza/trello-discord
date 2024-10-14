bcbe16b116227d73529509bbefa4e16d9c4b1c28
wdpza <simplexityza@gmail.com>
2024-10-14 18:34:22 +0200

    Fixed import

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+const axios = require('axios'); // CommonJS syntax
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/YOUR_WEBHOOK_ID/YOUR_WEBHOOK_TOKEN'; // Replace this
+const DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1295417038961115136/fsH0-K-oySgWFQ2YYouwyG_8DqEM8UJgbH7OWa-_E4-E69iMfQZK9RsHsCsTP3M0b51T';
 
 app.use(bodyParser.json());
 
 app.post('/trello-webhook', async (req, res) => {
     const { action } = req.body;
-
-    // Customize the message to send to Discord
     const message = {
         content: `**${action.memberCreator.fullName}** ${action.type} on card **${action.data.card.name}**`
     };
@@ -25,7 +24,6 @@ app.post('/trello-webhook', async (req, res) => {
     }
 });
 
-// Trello verification route to accept webhooks
 app.head('/trello-webhook', (req, res) => res.sendStatus(200));
 
 app.listen(PORT, () => {
